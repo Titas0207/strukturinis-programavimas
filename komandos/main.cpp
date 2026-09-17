b5fdcbffdb3e70f1f475315f6afc67d8fea99015
Titas Morkūnas <s067272@ad.viko.lt>
2026-09-17 11:41:32 +0300

    Atlikti veiksmai

---
+++ komandos/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string pavadinimas;
+
+    cout << "Iveskite sporto klubo pavadinima: ";
+    
+
+    return 0;
+};
